d7b5f72f7d4267a7f227a9c19ddc928d3602301b
Roopal98 <roopal.dumka@gmail.com>
2019-06-01 17:50:08 +0530

    Login Authentication added

---
+++ home.php
@@ -0,0 +1,24 @@
+<?php
+include "config.php";
+
+// Check user login or not
+if(!isset($_SESSION['uname'])){
+    header('Location: index.html');
+}
+
+// logout
+if(isset($_POST['but_logout'])){
+    session_destroy();
+    header('Location: index.html');
+}
+?>
+<!doctype html>
+<html>
+    <head></head>
+    <body>
+        <h1>Homepage</h1>
+        <form method='post' action="">
+            <input type="submit" value="Logout" name="but_logout">
+        </form>
+    </body>
+</html>
